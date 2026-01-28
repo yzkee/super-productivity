6f3a8902e2f309115680158be13afe508b980e75
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2026-01-28 15:37:50 +0100

    Increase ping interval and idle time in order to not waste CPU cycles. (#6227)
    If I understand the codebase correctly, this will do exactly as the title states, if not, please educate me :). Also is there a way to turn of idle checking when it is  also turned of in the settings?

--- electron/CONFIG.ts
+++ electron/CONFIG.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
   D_BUS_ID: 'com.super.productivity.service',
-  IDLE_PING_INTERVAL: 5000,
-  MIN_IDLE_TIME: 15000,
+  IDLE_PING_INTERVAL: 30000,
+  MIN_IDLE_TIME: 60000,
 };
