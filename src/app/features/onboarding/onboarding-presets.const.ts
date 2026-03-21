10fa8e70f23ad1352cde5cdb2fa926f40680d4d1
Johannes Millan <johannes.millan@gmail.com>
2026-03-21 14:17:22 +0100

    fix(onboarding): add missing isFinishDayEnabled to BASE_FEATURES

--- src/app/features/onboarding/onboarding-presets.const.ts
+++ src/app/features/onboarding/onboarding-presets.const.ts
@@ -22,6 +22,7 @@ const BASE_FEATURES: AppFeaturesConfig = {
   isDonatePageEnabled: true,
   isEnableUserProfiles: false,
   isHabitsEnabled: false,
+  isFinishDayEnabled: false,
 };
 
 export const ONBOARDING_PRESETS: OnboardingPreset[] = [
