a6a42c23cdceac37b81f03c71ff45e776ac6871b
Johannes Millan <johannes.millan@gmail.com>
2026-05-09 19:13:33 +0200

    Revert "refactor(sync): split sync coordinators"
    This reverts commit b1fd7de0b6c48379e1bd935eb94f9e13dcfcba83.

--- src/app/op-log/sync/operation-log-sync.service.spec.ts
+++ src/app/op-log/sync/operation-log-sync.service.spec.ts
@@ -34,8 +34,6 @@ import { SyncImportConflictDialogService } from './sync-import-conflict-dialog.s
 import { StateSnapshotService } from '../backup/state-snapshot.service';
 import { INBOX_PROJECT } from '../../features/project/project.const';
 import { TODAY_TAG, SYSTEM_TAG_IDS } from '../../features/tag/tag.const';
-import { SyncImportConflictCoordinatorService } from './sync-import-conflict-coordinator.service';
-import { ForceRemoteStateCoordinatorService } from './force-remote-state-coordinator.service';
 
 describe('OperationLogSyncService', () => {
   let service: OperationLogSyncService;
@@ -48,8 +46,6 @@ describe('OperationLogSyncService', () => {
   let superSyncStatusServiceSpy: jasmine.SpyObj<SuperSyncStatusService>;
   let stateSnapshotServiceSpy: jasmine.SpyObj<StateSnapshotService>;
   let syncImportConflictDialogServiceSpy: jasmine.SpyObj<SyncImportConflictDialogService>;
-  let syncImportConflictCoordinator: SyncImportConflictCoordinatorService;
-  let forceRemoteStateCoordinator: ForceRemoteStateCoordinatorService;
 
   beforeEach(() => {
     snackServiceSpy = jasmine.createSpyObj('SnackService', ['open']);
@@ -225,8 +221,6 @@ describe('OperationLogSyncService', () => {
     });
 
     service = TestBed.inject(OperationLogSyncService);
-    syncImportConflictCoordinator = TestBed.inject(SyncImportConflictCoordinatorService);
-    forceRemoteStateCoordinator = TestBed.inject(ForceRemoteStateCoordinatorService);
     // Default: not a fresh client
     opLogStoreSpy.loadStateCache.and.resolveTo({
       state: {},
@@ -2823,10 +2817,7 @@ describe('OperationLogSyncService', () => {
 
       syncImportConflictDialogServiceSpy.showConflictDialog.and.resolveTo('USE_LOCAL');
 
-      const forceUploadSpy = spyOn(
-        syncImportConflictCoordinator,
-        'forceUploadLocalState',
-      ).and.resolveTo();
+      const forceUploadSpy = spyOn(service, 'forceUploadLocalState').and.resolveTo();
 
       const mockProvider = {
         isReady: () => Promise.resolve(true),
@@ -2885,10 +2876,7 @@ describe('OperationLogSyncService', () => {
 
       syncImportConflictDialogServiceSpy.showConflictDialog.and.resolveTo('USE_REMOTE');
 
-      const forceDownloadSpy = spyOn(
-        forceRemoteStateCoordinator,
-        'forceDownloadRemoteState',
-      ).and.resolveTo();
+      const forceDownloadSpy = spyOn(service, 'forceDownloadRemoteState').and.resolveTo();
 
       const mockProvider = {
         isReady: () => Promise.resolve(true),
